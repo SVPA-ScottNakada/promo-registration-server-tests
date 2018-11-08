2c58f782f5e9071366b7575d48fba5a527d36413
Dan Alvarado <daniel.alvarado@sony.com>
2018-11-08 11:24:40 -0800

    created test data class

---
+++ src/test/java/com/promo/test/suite/registration_server/RegistrationServerTestData.java
@@ -0,0 +1,15 @@
+
+package com.promo.test.suite.registration_server;
+
+import com.promo.test.framework.utils.ConfigUtils;
+
+public class RegistrationServerTestData {
+
+    // Registration Server
+
+    public static final String REGISTRATION_SERVER_ENDPOINT =
+            ConfigUtils.getInstance().getValueFromEnvOrConfigFile("registration.server.baseurl");
+
+    public static final String REGISTRATION_SERVER_BASE_URI = REGISTRATION_SERVER_ENDPOINT + "promo/";
+
+}
